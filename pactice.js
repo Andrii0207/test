1331ae9692ac354aa73be89d9a536c067602f148
Andrii Popov <popov.andrey80@gmail.com>
2022-07-16 17:32:19 +0300

    update smth

--- pactice.js
+++ pactice.js
@@ -1,17 +1,27 @@
-const addListenerBtn = document.querySelector('[data-action="add"]');
-const removeListenerBtn = document.querySelector('[data-action="remove"]');
-const btn = document.querySelector('#btn');
+// const addListenerBtn = document.querySelector('[data-action="add"]');
+// const removeListenerBtn = document.querySelector('[data-action="remove"]');
+// const btn = document.querySelector('#btn');
 
-const handleClick = () => {
-  console.log('click event listener callback');
+// const handleClick = () => {
+//   console.log('click event listener callback');
+// };
+
+// addListenerBtn.addEventListener('click', () => {
+//   btn.addEventListener('click', handleClick);
+//   console.log('click event listener was added to btn');
+// });
+
+// removeListenerBtn.addEventListener('click', () => {
+//   btn.removeEventListener('click', handleClick);
+//   console.log('click event listener was removed from btn');
+// });
+
+const mango = {
+  username: 'Mango',
+  showUsername() {
+    console.log(this);
+    console.log(`My username is: ${this.username}`);
+  },
 };
 
-addListenerBtn.addEventListener('click', () => {
-  btn.addEventListener('click', handleClick);
-  console.log('click event listener was added to btn');
-});
-
-removeListenerBtn.addEventListener('click', () => {
-  btn.removeEventListener('click', handleClick);
-  console.log('click event listener was removed from btn');
-});
+const btn = document.querySelector('.js-btn');
