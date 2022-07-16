16062d67825f9aa9c463521846ef69562f73bd91
Andrii Popov <popov.andrey80@gmail.com>
2022-07-16 17:38:34 +0300

    add ColorPicker Task

--- pactice.js
+++ pactice.js
@@ -16,12 +16,54 @@
 //   console.log('click event listener was removed from btn');
 // });
 
-const mango = {
-  username: 'Mango',
-  showUsername() {
-    console.log(this);
-    console.log(`My username is: ${this.username}`);
-  },
-};
+/////////////////////////////////////
+/////////////////////////////////////
+/////////////////////////////////////
 
-const btn = document.querySelector('.js-btn');
+//////////// COLORPICKER ////////////
+
+/////////////////////////////////////
+/////////////////////////////////////
+
+const colorPalette = document.querySelector('.color-palette');
+const output = document.querySelector('.output');
+
+colorPalette.addEventListener('click', selectColor);
+
+// This is where delegation «magic» happens
+function selectColor(event) {
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const selectedColor = event.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+  output.style.color = selectedColor;
+}
+
+// Some helper functions to render palette items
+createPaletteItems();
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRangomColor();
+    const item = document.createElement('button');
+    item.type = 'button';
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add('item');
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+
+function getRangomColor() {
+  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+}
+
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, '0');
+}
