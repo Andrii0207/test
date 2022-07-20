493e84eda1ee7925eac2021b33feea2d041ad712
Andrii Popov <popov.andrey80@gmail.com>
2022-07-20 23:58:22 +0300

    add task Filter input

--- practice3.js
+++ practice3.js
@@ -18,11 +18,31 @@ const refs = {
   input: document.querySelector('#filter'),
 };
 
-const listItemsMarkup = createListItemsMarkup(tech);
-console.log(listItemsMarkup);
-
-refs.list.innerHTML = listItemsMarkup;
+refs.input.addEventListener('input', onFilterChange);
 
 function createListItemsMarkup(items) {
   return items.map(item => `<li>${item.label}</li>`).join('');
 }
+
+const listItemsMarkup = createListItemsMarkup(tech);
+console.log(listItemsMarkup);
+
+// refs.list.innerHTML = listItemsMarkup;
+populateList(listItemsMarkup);
+
+function onFilterChange(evt) {
+  const filter = evt.target.value.toLowerCase();
+  console.log(filter);
+  const filteredItems = tech.filter(t => t.label.toLowerCase().includes(filter));
+  console.log(filteredItems);
+  const listItemsMarkup = createListItemsMarkup(filteredItems);
+  console.log(listItemsMarkup);
+  // refs.list.innerHTML = listItemsMarkup;
+  populateList(listItemsMarkup);
+}
+
+function populateList(markup) {
+  refs.list.innerHTML = markup;
+}
+
+// populateList(listItemsMarkup);
