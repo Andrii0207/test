8df541be12ac5ac3d46562c6375df27fd8513558
Andrii Popov <popov.andrey80@gmail.com>
2022-07-22 21:35:19 +0300

    Update practice3.js

--- practice3.js
+++ practice3.js
@@ -13,36 +13,72 @@ const tech = [
   { label: 'MongoDB' },
 ];
 
+// const refs = {
+//   list: document.querySelector('.js-list'),
+//   input: document.querySelector('#filter'),
+// };
+
+// // refs.input.addEventListener('input', onFilterChange);
+
+// function createListItemsMarkup(items) {
+//   return items.map(item => `<li>${item.label}</li>`).join('');
+// }
+
+// const listItemsMarkup = createListItemsMarkup(tech);
+// console.log(listItemsMarkup);
+
+// refs.list.innerHTML = listItemsMarkup;
+
+// ////// refs.list.innerHTML = listItemsMarkup;///////
+// populateList(listItemsMarkup);
+
+// function onFilterChange(evt) {
+//   const filter = evt.target.value.toLowerCase();
+//   console.log(filter);
+//   const filteredItems = tech.filter(t => t.label.toLowerCase().includes(filter));
+//   console.log(filteredItems);
+//   const listItemsMarkup = createListItemsMarkup(filteredItems);
+//   console.log(listItemsMarkup);
+//   /////// refs.list.innerHTML = listItemsMarkup;///////
+//   populateList(listItemsMarkup);
+// }
+
+// function populateList(markup) {
+//   refs.list.innerHTML = markup;
+// }
+
+///////////////////////////////
+///////////////////////////////
+///////////////////////////////
+
 const refs = {
-  list: document.querySelector('.js-list'),
   input: document.querySelector('#filter'),
+  list: document.querySelector('.js-list'),
 };
 
 refs.input.addEventListener('input', onFilterChange);
 
-function createListItemsMarkup(items) {
+function onCreateListItems(items) {
   return items.map(item => `<li>${item.label}</li>`).join('');
 }
 
-const listItemsMarkup = createListItemsMarkup(tech);
+const listItemsMarkup = onCreateListItems(tech);
 console.log(listItemsMarkup);
 
 // refs.list.innerHTML = listItemsMarkup;
-populateList(listItemsMarkup);
+filteredList(listItemsMarkup);
 
 function onFilterChange(evt) {
   const filter = evt.target.value.toLowerCase();
   console.log(filter);
   const filteredItems = tech.filter(t => t.label.toLowerCase().includes(filter));
   console.log(filteredItems);
-  const listItemsMarkup = createListItemsMarkup(filteredItems);
+  const listItemsMarkup = onCreateListItems(filteredItems);
   console.log(listItemsMarkup);
   // refs.list.innerHTML = listItemsMarkup;
-  populateList(listItemsMarkup);
+  filteredList(listItemsMarkup);
 }
 
-function populateList(markup) {
+function filteredList(markup) {
   refs.list.innerHTML = markup;
 }
-
-// populateList(listItemsMarkup);
